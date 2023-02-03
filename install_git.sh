d29975d46e7a9ea6b4d62a1ed2b2bd7914765616
root <root@ubuntu-20.04-desktop-amd64.jesusamieiro.com>
2023-02-03 18:32:45 +0000

    Scripts de Git y Docker

---
+++ install_git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+add-apt-repository ppa:git-core/ppa
+sudo apt update -y
+sudo apt install -y git
+git -v
--- setup_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update -y
-snap install docker
-sudo gpasswd -a usuario docker
-git clone git@github.com:Robin118/docker_files.git
-su usuario
-cd docker_files
-docker-compose -f mysql.yml up -d
-docker ps
